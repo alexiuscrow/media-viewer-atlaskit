9e287a9d9eda7fadac7d8984b3dc786a7d9a2d07
Alexey Sapon <asapon@freeconferencecall.com>
2022-05-23 01:17:15 +0300

    Updated with Atlaskit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {IntlProvider} from 'react-intl';
+import {MediaViewer} from '@atlaskit/media-viewer';
 import './App.css';
 
+const imageUri = 'https://war.ukraine.ua/wp-content/uploads/2022/03/shutterstock_2029545419-2.png';
+const pdfUri = 'https://www.buds.com.ua/images/Lorem_ipsum.pdf';
+
 function App() {
+  const [imageDisplayed, setImageDisplayed] = useState(false);
+  const [pdfDisplayed, setPdfDisplayed] = useState(false);
+
+  const toggleImageMediaViewer = () => setImageDisplayed(prevState => !prevState);
+  const togglePdfMediaViewer = () => setPdfDisplayed(prevState => !prevState);
+
+  let mediaViewer = null;
+
+  if (imageDisplayed) {
+    const mediaIdentifier = {
+      mediaItemType: 'external-image',
+      name: 'Some image',
+      dataURI: imageUri
+    };
+    mediaViewer = (
+      <MediaViewer mediaClientConfig={{}}
+                   selectedItem={mediaIdentifier}
+                   dataSource={{list: [mediaIdentifier]}}
+                   collectionName={''}
+                   onClose={toggleImageMediaViewer}/>
+    );
+  } else if (pdfDisplayed) {
+    const mediaIdentifier = {
+      mediaItemType: 'file',
+      name: 'Some PDF',
+      dataURI: pdfUri
+    };
+    mediaViewer = (
+      <MediaViewer mediaClientConfig={{}}
+                   selectedItem={mediaIdentifier}
+                   dataSource={{list: [mediaIdentifier]}}
+                   collectionName={''}
+                   onClose={togglePdfMediaViewer}/>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Hello Atlassian Community</h3>
+
+      <button onClick={toggleImageMediaViewer}>Show Image</button>
+      <button onClick={togglePdfMediaViewer}>Show PDF</button>
+
+      <IntlProvider locale="en">
+        <>
+          {mediaViewer}
+        </>
+      </IntlProvider>
     </div>
   );
 }
